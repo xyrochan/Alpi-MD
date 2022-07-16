e07d5f2775524e88649cef0f8deccac3f7c17adf
xyrochan <108439460+xyrochan@users.noreply.github.com>
2022-07-16 12:50:54 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6285850539404'],
-  ['6285850539404'],
-  ['6285850539404', 'Owner', true]
+  ['6287837703726'],
+  ['6285945646584'],
+  ['6287837703726', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = [6287837703726] // Want some help?
+global.prems = [6287837703726] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
